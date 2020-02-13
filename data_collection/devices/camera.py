93623f12e465d1e7dd61bd602959473bec501d33
nubby <nubby@funkle.milk.net>
2020-02-12 22:55:18 -0800

    made Camera a child class of Device

--- data_collection/devices/camera.py
+++ data_collection/devices/camera.py
@@ -1,14 +1,19 @@
 """
 camera.py - Webcam API for the FUTURE.
 Radish'n'bots, LLC
-  modified : 1/27/2020
+     ) 0 o .
+    modified : 2/12/2020
 """
 import time
 try:
-  import picamera
+    import picamera
 except ImportError:
   print(str(ImportError))
   import cv2 as picamera
+try:
+    from .device import Device
+except ImportError:
+    from device import Device
 import os
 import io
 import sys
@@ -70,125 +75,121 @@ def _verify_cv2_port(port_index):
 
 
 # TODO: Import from Device class.
-class Camera(object):
-  def __init__(
-      self, device_address,
-      serial_number='', device_type='usb_camera'):
-    ## TODO: Integrate below into generic Device class.
-    self.device_address = str(device_address)   # Save device address as a string.
-    self.base_data_path = './'
-    setup_info = {
-        'serial_number': serial_number,
-        'device_type': device_type
-        }
-    # Fill in the blanks.
-    self.info = self._query_info(setup_info)
-    self.info['device_id']= self._get_device_id()
-    ##
+class Camera(Device):
+    def __init__(
+          self, device_address,
+          serial_number='', device_type='usb_camera'):
+        try:
+            super().__init__(device_address, serial_number, device_type)
+        except:
+            super(Camera, self).__init__(device_address, serial_number, device_type)
 
-  def _query_info(self, old_info={}):
-    """
-    Chat up the device to find where it lives as well
-      as how to get into its front door.
-    :in: old_info {dict} - any old metadata 'bout the device.
-    :out: info {dict}
-    """
-    info = {
-        'serial_number': '1',
-        'device_type': 'usb_camera',
-        'image_format': 'png',
-        'device_address': self.device_address,
-        'resolution': '720p',
-        'framerate': 15
-        }
-    info.update(old_info)
-    return info
+    def _query_info(self, old_info={}):
+        """
+        Chat up the device to find where it lives as well
+          as how to get into its front door.
+        :in: old_info {dict} - any old metadata 'bout the device.
+        :out: info {dict}
+        """
+        info = {
+            'serial_number': '1',
+            'device_type': 'usb_camera',
+            'image_format': 'png',
+            'device_address': self.device_address,
+            'resolution': '720p',
+            'framerate': 15
+            }
+        info.update(old_info)
+        print(info)
+        return info
 
-  def _get_device_id(self):
-    """
-    Hunt down the device ID.
-    :out: serial_number (str)
-    """
-    unique_ids = [
-        self.info['device_type'],
-        self.info['serial_number'],
-        self.info['device_address'].replace('/','_')]
-    return '-'.join(unique_ids)
+    def _get_device_id(self):
+        """
+        Hunt down the device ID.
+        :out: serial_number (str)
+        """
+        unique_ids = [
+            self.info['device_type'],
+            self.info['serial_number'],
+            self.info['device_address'].replace('/','_')]
+        return '-'.join(unique_ids)
 
 
-  def set_up(self):
-    """
-    Verify device properly connected:
-      1. Verify that address of device specifies a valid OpenCV index.
-    """
-    available = False
-    available = _verify_cv2_port(int(self.device_address))
-    if not available:
-      print(''.join([
-        'ERROR   : Unable to communicate with camera source ',
-        self.device_address,'!']))
-      raise IOError
+    def set_up(self):
+        """
+        Verify device properly connected:
+          1. Verify that address of device specifies a valid OpenCV index.
+        """
+    #    available = False
+    #    available = _verify_cv2_port(int(self.device_address))
+    #    if not available:
+    #      print(''.join([
+    #        'ERROR   : Unable to communicate with camera source ',
+    #        self.device_address,'!']))
+    #      raise IOError
+    #
+        print('Setting up from child!')
 
-  def set_data_path(self, path_base):
-    """
-    Set the base of the output path for data flow.
-    :in: path_base (str)
-    """
-    if os.path.isdir(path_base):
-      self.base_data_path = path_base
-    else:
-      print(''.join([
-        'WARNING : Cannot set path base to ',
-        str(path_base), '; directory does not exist! Using next deepest...']))
+    def set_data_path(self, path_base):
+        """
+        Set the base of the output path for data flow.
+        :in: path_base (str)
+        """
+        if os.path.isdir(path_base):
+          self.base_data_path = path_base
+        else:
+          print(''.join([
+            'WARNING : Cannot set path base to ',
+            str(path_base), '; directory does not exist! Using next deepest...']))
 
-  def take_picture(self, label='test'):
-    """
-    Camera specific method!
-    """
-    ## Get resolution.
-    try:
-      resolution = self.info['resolution']
-      frame_w = _RESOLUTIONS[resolution]['width']
-      frame_h = _RESOLUTIONS[resolution]['height']
-    except:
-      print(''.join([
-      'WARNING : Resolution ', self.info['resolution'],
-      ' unsupported; default to ', _DEFAULT['resolution'],'.']))
-      resolution = _DEFAULT['resolution']
-      frame_w = _RESOLUTIONS[resolution]['width']
-      frame_h = _RESOLUTIONS[resolution]['height']
+    def take_picture(self, label='test'):
+        """
+        Camera specific method!
+        """
+        ## Get resolution.
+        try:
+            resolution = self.info['resolution']
+            frame_w = _RESOLUTIONS[resolution]['width']
+            frame_h = _RESOLUTIONS[resolution]['height']
+        except:
+            print(''.join([
+            'WARNING : Resolution ', self.info['resolution'],
+            ' unsupported; default to ', _DEFAULT['resolution'],'.']))
+            resolution = _DEFAULT['resolution']
+            frame_w = _RESOLUTIONS[resolution]['width']
+            frame_h = _RESOLUTIONS[resolution]['height']
 
-    ## Get image format.
-    image_format = self.info['image_format']
-    if not image_format in _FORMATS['image']:
-      print(''.join([
-      'WARNING : Picture format ', self.info['image_format'],
-      ' unsupported; default to ', _DEFAULT['image_format'],'.']))
-      image_format = _DEFAULT['image_format']
+        ## Get image format.
+        image_format = self.info['image_format']
+        if not image_format in _FORMATS['image']:
+            print(''.join([
+            'WARNING : Picture format ', self.info['image_format'],
+            ' unsupported; default to ', _DEFAULT['image_format'],'.']))
+            image_format = _DEFAULT['image_format']
 
-    ## Generate image label.
-    timestamp = _get_time_now('timestamp')
-    image_label = '.'.join([
-      '-'.join([
-        self.base_data_path+timestamp,
-        str(self.device_address),
-        label]),
-      image_format])
+        ## Generate image label.
+        timestamp = _get_time_now('timestamp')
+        image_label = '.'.join([
+            '-'.join([
+                self.base_data_path+timestamp,
+                str(self.device_address),
+                label]),
+            image_format])
     
-    ## Take a pic.
-    cam = cv2.VideoCapture(int(self.device_address))
-    ret, frame = cam.read()
-    cv2.imwrite(image_label, frame)
-    cam.release()
+        ## Take a pic.
+        cam = cv2.VideoCapture(int(self.device_address))
+        ret, frame = cam.read()
+        cv2.imwrite(image_label, frame)
+        cam.release()
 
-    ## Verify that file exists.
-    # TODO
+        ## Verify that file exists.
+        # TODO
     
-  def clean_up(self):
-    """
-    Take out the trash.
-    """
-    return
+    def clean_up(self):
+        """
+        Take out the trash.
+        """
+        return
   
 
 class Webcam(object):
@@ -260,26 +261,30 @@ def _get_available_camera_indices():
   return available_indices
 
 def test_camera():
-  # Set up.
-  base_dc_dir = './test_data/'
-  if not os.path.isdir(base_dc_dir):
-    os.mkdir(base_dc_dir)
-  # Find each available camera.
-  available_camera_indices = _get_available_camera_indices()
-  device_address = max(available_camera_indices)
+    # Set up.
+    base_dc_dir = './test_data/'
+    if not os.path.isdir(base_dc_dir):
+        os.mkdir(base_dc_dir)
+    # Find each available camera.
+    available_camera_indices = _get_available_camera_indices()
+    try:
+        device_address = max(available_camera_indices)
+    except:
+        print('No cameras connected!')
+        device_address = 0
 
-  # Test the camera.
-  cam = Camera(device_address)
-  cam.set_up()
-  cam.set_data_path(base_dc_dir)
-  time.sleep(10)
-  cam.take_picture()
-  time.sleep(5)
-  cam.take_picture('yellow_oyster')
-  cam.clean_up()
+    # Test the camera.
+    cam = Camera(device_address)
+    cam.set_up()
+    cam.set_data_path(base_dc_dir)
+    time.sleep(10)
+    cam.take_picture()
+    time.sleep(5)
+    cam.take_picture('yellow_oyster')
+    cam.clean_up()
 
 if __name__ == '__main__':
-  test_camera()
+    test_camera()
 
 #cam = Webcam()
 #cam.timelapse_photos(interval=5, image_name='test', length_of_time=60)
