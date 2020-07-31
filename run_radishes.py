661d508e5ba3a3757497b4f5eb42cc320e822c1a
nubbysaurus <nubby@mail.com>
2020-07-30 17:11:49 -0700

    added run_radishes

---
+++ run_radishes.py
@@ -0,0 +1,13 @@
+"""
+Main script for radishes.
+     ) 0 o .
+    modified : 7/30/2020
+"""
+from sableye import .sableye.Sableye
+
+
+def run_radishes():
+    pass
+
+if __name__ == '__main__':
+    run_radishes()
